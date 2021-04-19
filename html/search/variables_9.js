ef1a26416530dfb29caf155df775ab52633b37f5
Doxygen CI <ci-arduino@invalid>
2021-04-19 11:09:19 +0000

    Deploy docs to GitHub Pages from commit e4932b6ac2
    Commit: e4932b6ac2316ab83b0b38c4e5932a9cc4572697
    GitHub Actions run: 763315090

--- html/search/variables_9.js
+++ html/search/variables_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['radius',['radius',['../class_adafruit___g_f_x___button.html#a1492c1850570e21da5e266715f1c3933',1,'Adafruit_GFX_Button']]],
   ['rotation',['rotation',['../class_adafruit___g_f_x.html#a37a479d28fb11906ce516e983b1af926',1,'Adafruit_GFX']]],
   ['rstpin',['rstPin',['../class_adafruit___gray_o_l_e_d.html#a5b8ad07d90e716428259fb6ed22fbd3d',1,'Adafruit_GrayOLED']]]
 ];
